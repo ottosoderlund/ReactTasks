07c617a630828d1f17add33f95858f78600fcd9e
ottosoderlund <otto.soderlund@normipaivaoy.fi>
2019-09-02 14:21:58 +0300

    first commit through github

---
+++ tue.27.8/App.js
@@ -0,0 +1,29 @@
+/* eslint-disable max-len */
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+import {MediaProvider} from './contexts/MediaContext';
+import List from './components/List';
+
+
+const App = () => {
+  return (
+    <MediaProvider>
+      <View style={styles.container}>
+        <List />
+      </View>
+    </MediaProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    paddingHorizontal: 10,
+    paddingTop: 40,
+  },
+});
+
+export default App;

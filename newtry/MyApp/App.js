37d4282115bb4c9f6d9ec1c57bcff9dfe8be4006
ottosoderlund <otto.soderlund@normipaivaoy.fi>
2019-09-10 11:08:45 +0300

    Retry after react internal file error

---
+++ newtry/MyApp/App.js
@@ -0,0 +1,15 @@
+/* eslint-disable max-len */
+import React from 'react';
+import {MediaProvider} from './contexts/MediaContext';
+import Navigator from './navigators/Navigator';
+
+
+const App = () => {
+  return (
+      <MediaProvider>
+        <Navigator></Navigator>
+      </MediaProvider>
+  );
+};
+
+export default App;
